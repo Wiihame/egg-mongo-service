974c733ef1db5a92cd5708f6fc58118e639dbf20
Wiihame <wiihame@outlook.com>
2019-05-15 13:41:10 +0800

    add user-controller

--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,15 @@
 'use strict';
 
 /** @type Egg.EggPlugin */
-module.exports = {
+exports.nunjucks = {
   // had enabled by egg
   // static: {
-  //   enable: true,
+  enable: true,
+  package: 'egg-view-nunjucks',
   // }
+};
+
+exports.mongoose = {
+  enable: true,
+  package: 'egg-mongoose',
 };
